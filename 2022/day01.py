ed9c4b09abb76a6287f01d871e5b4fb0d008594a
Jernek <82438182+Jernek@users.noreply.github.com>
2022-12-01 22:14:08 +0100

    minor modifications

--- 2022/day01.py
+++ 2022/day01.py
@@ -6,17 +6,15 @@ def get_Data():
             data.append([int(item) for item in groups.splitlines()])
     return data
 
+def part1(Input):
+    return max(Input)
 
-def part1(p1_Data):
-    Solution_1 = max([sum(vector) for vector in p1_Data])
-    return Solution_1
-
-def part2(p2_Data): 
-    Solution2 = [sum(vector) for vector in p2_Data]
-    return (sum(sorted(Solution2)[-3:]))
-
+def part2(Input): 
+    return (sum(sorted(Input)[-3:]))
+    
 
 if __name__ == "__main__":
     data = get_Data()
-    print(f"Solution to First star: {part1(data)}")
-    print(f"\nSolution to Second star: {part2(data)}")
+    Input = [sum(vector) for vector in data]
+    print(f"Solution to First star: {part1(Input)}")
+    print(f"\nSolution to Second star: {part2(Input)}")
